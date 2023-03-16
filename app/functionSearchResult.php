7ed3a57dd01c35da7a6f01306b5ae2719322e2ac
testuser <canda3267@gmail.com>
2023-03-16 23:18:43 +0900

    [remove] removed countNumber

--- app/functionSearchResult.php
+++ app/functionSearchResult.php
@@ -1,14 +1,9 @@
 <div class="container">
     <?php for ($i = 0; $i <= count($searchResultArray) - 1; $i++) : ?>
-        <div class="unit-container">
-            <div class="count">
-                <p><?php echo ($i + 1) . "件目" ?></p>
-            </div>
-            <div class="content">
-                <?php $contentString = $searchResultArray[$i]["sentences"] ?>
-                <p><?php echo nl2br($contentString) ?></p>
-            </div>
-            <br>
+        <div class="content">
+            <?php $contentString = $searchResultArray[$i]["sentences"] ?>
+            <p><?php echo nl2br($contentString) ?></p>
         </div>
+        <br>
     <?php endfor ?> <br>
 </div>
