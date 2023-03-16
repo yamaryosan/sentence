1ab0e35792a33ea86d220b10764b37ae9ae28a1d
testuser <canda3267@gmail.com>
2023-03-17 00:58:44 +0900

    [fix] 結果ページへのフィラー追加

--- app/functionSearchResult.php
+++ app/functionSearchResult.php
@@ -3,6 +3,7 @@
         <div class="content">
             <?php $contentString = $searchResultArray[$i]["sentences"] ?>
             <p><?php echo nl2br($contentString) ?></p>
+            <p><?php require("previewPageFiller.php") ?></p>
         </div>
         <br>
     <?php endfor ?> <br>
