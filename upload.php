5cb01295da68fe063c2426c72394ed0ed48ac0a0
testuser <canda3267@gmail.com>
2023-03-19 16:44:59 +0900

    [add]アップロードページを追加

---
+++ upload.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+require_once("../app/class/classUploader.php");
+require_once("../app/class/classDataSaver.php");
+
+header("Cache-Control: no-cache, must-revalidate");
+
+// アップロードを受け付ける
+if (isset($_FILES["uploaded-file"]) === true) {
+    $uploader = new Uploader($_FILES["uploaded-file"]);
+    $uploader->upload();
+    $pythonFileName = "../docx_to_text.py";
+    // pyファイルを実行してドキュメントファイルをテキストファイルに変換
+    exec("python " . $pythonFileName . " 2>&1", $output, $state);
+    if ($state !== 0) {
+        echo "エラーがあります。" . PHP_EOL;
+        print_r(mb_convert_encoding($output, "UTF-8", "sjis"));
+    } else {
+        echo "変換成功" . PHP_EOL;
+    }
+    // テキストファイルを読み取って保存
+    $dataSaver = new DataSaver("../uploads/uploadedTextFile.txt");
+    $dataSaver->save();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style-upload.css">
+</head>
+
+<body>
+    <form action="uploadPage.php" method="post" enctype="multipart/form-data">
+        <input type="file" class="file-dropzone" name="uploaded-file">
+        <input type="submit" class="submit-btn" value="アップロード">
+    </form>
+    <button onclick="location.href='searchPage.php'">検索ページへ移動</button>
+</body>
+
+</html>
