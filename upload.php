cf0b8de7057814943615de4cdd46a0095c7f107b
testuser <canda3267@gmail.com>
2023-03-23 22:30:05 +0900

    [add] テキストファイルが存在する場合、DBに保存する機能を追加

--- upload.php
+++ upload.php
@@ -3,23 +3,26 @@
 declare(strict_types=1);
 require_once("./app/classUploader.php");
 require_once("./app/classDataSaver.php");
+require_once("./app/classDocxToTxtConverter.php");
+set_time_limit(3600);
 
 header("Cache-Control: no-cache, must-revalidate");
+
+// テキストファイルが存在する場合はそれをDBに保存
+if (file_exists("./uploads/uploadedTextFile.txt") === true) {
+    $dataSaver = new DataSaver("./uploads/uploadedTextFile.txt");
+    $dataSaver->save();
+    echo "<script>alert('テキストファイルが既にあったため、DBに保存しました。');</script>";
+}
 
 // アップロードを受け付ける
 if (isset($_FILES["uploaded_file"]) === true) {
     $uploader = new Uploader($_FILES["uploaded_file"]);
     $uploader->upload();
-    $pythonFileName = "./app/docx_to_txt.py";
-    // pyファイルを実行してドキュメントファイルをテキストファイルに変換
-    exec("python " . $pythonFileName . " 2>&1", $output, $state);
-    if ($state !== 0) {
-        echo "エラーがあります。" . PHP_EOL;
-        print_r(mb_convert_encoding($output, "UTF-8", "sjis"));
-    } else {
-        echo "変換成功" . PHP_EOL;
-    }
-    // テキストファイルを読み取って保存
+    // ドキュメントファイルをテキストファイルに変換
+    $docxToTxtConverter = new DocxToTxtConverter("./uploads/uploadedDocxFile.docx", "./uploads/uploadedTextFile.txt");
+    $docxToTxtConverter->convert();
+    // テキストファイルを読み取ってDBに保存
     $dataSaver = new DataSaver("./uploads/uploadedTextFile.txt");
     $dataSaver->save();
 }
@@ -31,13 +34,13 @@ if (isset($_FILES["uploaded_file"]) === true) {
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style_upload.css">
+    <link rel="stylesheet" href="./css/style_upload.css">
 </head>
 
 <body>
     <form action="upload" method="post" enctype="multipart/form-data">
         <input type="file" class="file_dropzone" name="uploaded_file">
-        <input type="submit" class="submit-btn" value="アップロード">
+        <input type="submit" class="submit_btn" value="アップロード">
     </form>
     <a href="top.php">戻る</a>
     <a href="delete.php">DB削除</a>
